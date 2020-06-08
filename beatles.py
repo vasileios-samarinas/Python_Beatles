c6b7b446bbd71af2592904635bd0d1cfe20e7f0c
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-06-08 17:42:22 +0100

    Question 3 solved

--- beatles.py
+++ beatles.py
@@ -21,3 +21,20 @@ def names(members):
 
 result = names(beatles)
 print(result)
+
+# 3. Write a function which takes in the list of band members as a parameter,
+#    and returns a list of the members who are still alive
+#    (i.e. they have no value for `death_year`)
+#    Return the full dictionary for each member
+# Expected result: [
+#    {'name': 'Paul McCartney', 'birth_year': 1942, 'death_year': None, 'instrument': 'bass'},
+#    {'name': 'Ringo Starr', 'birth_year': 1940, 'death_year': None, 'instrument': 'drums'}
+# ]
+
+def alivemembers(members):
+    return [member for member in members if member['death_year'] == None]
+
+result = alivemembers(beatles)
+print(result)
+
+
